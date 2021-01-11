74cab9513029bf855bcbc69e91c998408a635f2f
Jack318 <63045343+Jack318@users.noreply.github.com>
2021-01-11 15:17:15 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,24 +4,31 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     timer = 0
-    while (!(input.buttonIsPressed(Button.B))) {
+    while (true) {
         timer += 1
         basic.showString("" + (timer))
-        break;
+        if (input.logoIsPressed()) {
+            break;
+        } else {
+            continue;
+        }
     }
     basic.showIcon(IconNames.StickFigure)
+    basic.pause(5000)
+    basic.showString("BYE")
 })
 let timer = 0
 basic.showString("Hiya")
 music.playMelody("C5 B A G F E D C ", 120)
-for (let index = 0; index < 5; index++) {
+for (let index = 0; index < 3; index++) {
+    basic.showIcon(IconNames.Chessboard)
     basic.showIcon(IconNames.Heart)
     basic.showIcon(IconNames.SmallHeart)
     basic.showIcon(IconNames.Square)
     basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Target)
     basic.showIcon(IconNames.Diamond)
     basic.showIcon(IconNames.SmallDiamond)
-    basic.showIcon(IconNames.Chessboard)
     basic.clearScreen()
 }
 basic.forever(function () {
